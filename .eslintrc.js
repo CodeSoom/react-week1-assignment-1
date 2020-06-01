31f61200d1905c43494993c1b4630ab378ddf85d
wholemann <wholeman.dev@gmail.com>
2020-06-01 09:10:33 +0900

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    actor: 'readonly',
+    Feature: 'readonly',
+    Scenario: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['codecept.conf.js'] }
+    ]
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+/* @jsx createElement */
