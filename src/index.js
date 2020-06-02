6083d3ea6acb27b5a7dcb33e91bd73c5bf5a7b0f
Minwook <minwook.park@enernoc.com>
2020-06-03 00:45:11 +0900

    lint failed. test failed. "I click 2"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function handleClick(value = 0) {
+  value += 1;
+  render(value);
+}
+
+function render(cnt = 0) {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(cnt)}>
+          Click me!
+          (
+            {cnt}
+          )
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
