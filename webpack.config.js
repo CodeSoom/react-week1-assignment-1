6d4bfa8f8d0d7b9af21efe4306e866c4eea25e16
eonsang <2eonhworking@gmail.com>
2020-06-03 04:26:32 +0900

    Remove codes
    Remove unintentionally merged contents.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
