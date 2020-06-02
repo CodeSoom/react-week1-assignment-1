060707292bf1b0f91056398132961140c28adf96
jiunkoo <jiunkoo0106@gmail.com>
2020-06-03 00:14:03 +0900

    index.js 기본 만들기

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
-
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const newElement = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    newElement[key.toLowerCase()] = value;
+  });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      newElement.appendChild(child);
+      return;
+    }
+    newElement.appendChild(document.createTextNode(child)); 
+  });
+
+  return newElement;
+}
+
+let count = 0;
+
+function handleClick() {
+  count += 1;
+  render();
+}
+
+function handleClickNumber(value) {
+  count = value;
+  render();
+}
+
+const render = function(){
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me! (
+            {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick ={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
