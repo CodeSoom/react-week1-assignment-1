2be5090cb77b1c7a74da81395e6c2493adab1562
sogoagain <imyong0@gmail.com>
2020-06-03 01:09:07 +0900

    JSX로 parent-child 구조를 갖는 기본적인 DOM 그리기
    JSX 렌더링을 위한 createElement 함수의 기본적인 구조를 작성하였다. (props에 대해서는 미구현)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 
 /* @jsx createElement */
+const createElement = (tagName, props, ...children) => {
+  const element = document.createElement(tagName);
+  const childNodes = [...children].map((child) => {
+    if (child instanceof Node) {
+      return child;
+    }
+    return document.createTextNode(child);
+  });
+  [...childNodes].forEach((node) => element.appendChild(node));
+  return element;
+};
+
+const element = (
+  <div>
+    <p>Hello World!</p>
+    <div>
+      <p>
+        Hi!
+      </p>
+    </div>
+  </div>
+);
+
+const container = document.getElementById('app');
+container.appendChild(element);
