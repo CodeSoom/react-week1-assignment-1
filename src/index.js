910db26d11834ce9bd564b98aa20511880c6501d
Kwakcena <rhkrgudwh@gmail.com>
2020-06-02 22:44:59 +0900

    Class로 묶어서 let을 제거함.
    let을 제거하기 위해서 App 이라는 클래스로 묶었다. this를 사용해서 count
    변수에 접근할 수 있고, 각각의 메서드에도 접근이 가능하다.
    
    사실 this에 대해 자세히 알지 모르지만, 최근 우아한테크캠프 2차
    코딩테스트 과제를 했던 것이 생각이 나서 작성 해 보았다.
    
    동작은 잘 되지만, 문제는 lint의 에러로 아래의 에러가 나타난다.
    
    error : Do not use 'new' for side effects

--- src/index.js
+++ src/index.js
@@ -19,37 +19,42 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-let count = 0;
+class App {
+  constructor() {
+    this.count = 0;
+    this.render();
+  }
 
-function handleClick() {
-  count += 1;
-  render();
+  handleClick() {
+    this.count += 1;
+    this.render();
+  }
+
+  handleClickNumber(value) {
+    this.count = value;
+    this.render();
+  }
+
+  render() {
+    const element = (
+      <div id="hello" className="greeting">
+        <p>Remove let</p>
+        <button type="button" onClick={() => this.handleClick()}>
+          Click me! (
+          {this.count}
+          )
+        </button>
+        <p>
+          {[1, 2, 3].map((i) => (
+            <button type="button" onClick={() => this.handleClickNumber(i)}>{i}</button>
+          ))}
+        </p>
+      </div>
+    );
+
+    document.getElementById('app').textContent = '';
+    document.getElementById('app').appendChild(element);
+  }
 }
 
-function handleClickNumber(value) {
-  count = value;
-  render();
-}
-
-function render() {
-  const element = (
-    <div id="hello" className="greeting">
-      <p>Remove let</p>
-      <button type="button" onClick={() => handleClick()}>
-        Click me! (
-        {count}
-        )
-      </button>
-      <p>
-        {[1, 2, 3].map((i) => (
-          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
-        ))}
-      </p>
-    </div>
-  );
-
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
-}
-
-render();
+new App(document.querySelector('app'));
