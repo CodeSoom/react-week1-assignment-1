0883039bab24022f999ce6453efd09395361d901
JangGeonil <>
2020-06-02 22:36:28 +0900

    기존 강의 코드로 다시 복원 작업 001

--- src/index.js
+++ src/index.js
@@ -1,49 +1,32 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
 
-
-const mainContainer = document.querySelector('#app');
-
-// eslint-disable-next-line no-unused-vars
 function createElement(tagName, props = {}, ...children) {
   const element = document.createElement(tagName);
-  Object.entries(props || {}).forEach(([key, value]) => { element[key.toLowerCase()] = value; });
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key] = value;
+  });
   children.flat().forEach((child) => {
     if (child instanceof Node) {
-      return element.appendChild(child);
+      element.appendChild(child);
+      return;
     }
-    return element.appendChild(document.createTextNode(child));
+    element.appendChild(document.createTextNode(child));
   });
   return element;
 }
 
 
 function render() {
-  function handleClick(number = 0) {
-    const target = document.querySelector('#count');
-    let value = '';
-    if (number) {
-      value = number;
-    } else {
-      value = parseInt(target.textContent, 10);
-      value += 1;
-    }
-    target.textContent = value.toString();
-  }
   const element = (
-    <div id="hello" className="greeting">
-      <p>Count: </p>
-      <span id="count">0</span>
-      <p>
-        <button type="button" onClick={() => handleClick()}>Click Me!</button>
-        {[1, 2, 3].map((number) => <button type="button" onClick={() => handleClick(number)}>{number}</button>)}
-      </p>
-    </div>
+    <p>
+      <h1>Hello world</h1>
+    </p>
   );
 
-  mainContainer.textContent = '';
-  mainContainer.appendChild(element);
+  document.querySelector('#app').textContent = '';
+  document.querySelector('#app').appendChild(element);
 }
-
 
 render();
