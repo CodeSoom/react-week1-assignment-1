2265c8c17bc0d054e907d1fdc3bae34b838e18a1
sogoagain <imyong0@gmail.com>
2020-06-03 01:25:45 +0900

    JSX로 DOM을 그릴 때 props 할당
    createElement 함수에서 props를 전달받아 DOM에 추가하는 로직 구현

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@
 /* @jsx createElement */
 const createElement = (tagName, props, ...children) => {
   const element = document.createElement(tagName);
+  const propEntries = Object.entries(props || {});
   const childNodes = [...children].map((child) => {
     if (child instanceof Node) {
       return child;
     }
     return document.createTextNode(child);
   });
+
+  propEntries.forEach(([key, value]) => {
+    element[key] = value;
+  });
   [...childNodes].forEach((node) => element.appendChild(node));
   return element;
 };
 
 const element = (
-  <div>
+  <div id="hello-world" className="greeting">
     <p>Hello World!</p>
-    <div>
-      <p>
-        Hi!
-      </p>
-    </div>
   </div>
 );
 
