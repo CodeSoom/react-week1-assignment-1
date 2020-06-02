d232f29763ff34dbfb09e978fb5c1f82d6734f25
eonsang <2eonhworking@gmail.com>
2020-06-03 04:58:52 +0900

    과제 완료

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
 
-const createElement = (tagName, ...children) => {
+const createElement = (tagName, props, ...children) => {
   const element = document.createElement(tagName);
-  console.log(children);
-  console.log('--------------------------');
-  children.forEach((child) => {
-    console.log(child);
-    element.appendChild(child);
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
   });
-  console.log('--------------------------');
-  console.log(element);
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
   return element;
 };
 
-document
-  .getElementById('app')
-  .appendChild(
-    createElement(
-      'div',
-      ...[1, 2, 3].map((i) => document.createTextNode(`Hello, world!${i} `)),
-      createElement('p', document.createTextNode('Hello, world!!!!\n'), document.createTextNode('Hi!!! \n')),
-      createElement('p', document.createTextNode('Hi!!!!!!!!!!!!\n'), document.createTextNode('Hi22222222!!! \n')),
-    ),
+function handleClick(value) {
+  value += 1;
+  render(value);
+}
+function handleClickNumber(value) {
+  render(value);
+}
+function render(count = 0) {
+  const element = (
+    <div id="wrapper" className="greeting hello">
+      123123
+      <p className="description">Hello, world!</p>
+      <button type="button" onClick={() => handleClick(count)}>
+        Click me! ({count})
+      </button>
+      <div>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </div>
+    </div>
   );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
