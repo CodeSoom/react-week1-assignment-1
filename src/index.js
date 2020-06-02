6eedfe4912549ab243b0da3a9731036d7a0554e4
Kwakcena <rhkrgudwh@gmail.com>
2020-06-03 00:40:17 +0900

    count 데이터를 객체로 관리하기
    let 으로 선언되어 있는 data를 const 로 선언된 객체로 만들어 내부의
    프로퍼티로 count값을 관리 한다.
    
    자바스크립트의 object는 참조형이므로 하나의 객체가 할당되면 다른 주소의
    객체로 바꿀 수 없지만 내부의 프로퍼티는 수정 가능하다.
    
    - no-use-before-define은 어떻게 해결하는가?
    : render 함수 내에 handleClick과 handleClickNumber 함수를 넣었다.
    render가 미리 선언된 뒤에 수행할 수 있도록 한다.
    
    - createElement is defined but never used는 어떻게 해결하는가?
    : 첫번째 리뷰에서 윤석님이 말씀하셨다.
    "createElement"를 직접 사용하진 않지만 필요한 함수죠. 하지만 eslint는
    이걸 모릅니다.
    그래서 알게 해줬다. 주석을 달아주면 해당 라인에만 no-unused-vars 규칙을
    적용할 수 있다.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 
 /* @jsx createElement */
-function createElement(tagName, props, ...children) {
+function createElement(tagName, props, ...children) { // eslint-disable-line no-unused-vars
   const element = document.createElement(tagName);
 
   Object.entries(props || {}).forEach(([key, value]) => {
@@ -19,42 +19,39 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-class App {
-  constructor() {
-    this.count = 0;
-    this.render();
+const data = {
+  count: 0,
+};
+
+function render() {
+  function handleClick() {
+    data.count += 1;
+    render();
   }
 
-  handleClick() {
-    this.count += 1;
-    this.render();
+  function handleClickNumber(value) {
+    data.count = value;
+    render();
   }
 
-  handleClickNumber(value) {
-    this.count = value;
-    this.render();
-  }
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Remove let</p>
+      <button type="button" onClick={() => handleClick()}>
+        Click me! (
+        {data.count}
+        )
+      </button>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))}
+      </p>
+    </div>
+  );
 
-  render() {
-    const element = (
-      <div id="hello" className="greeting">
-        <p>Remove let</p>
-        <button type="button" onClick={() => this.handleClick()}>
-          Click me! (
-          {this.count}
-          )
-        </button>
-        <p>
-          {[1, 2, 3].map((i) => (
-            <button type="button" onClick={() => this.handleClickNumber(i)}>{i}</button>
-          ))}
-        </p>
-      </div>
-    );
-
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
-  }
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
-(() => new App(document.querySelector('app')))();
+render();
