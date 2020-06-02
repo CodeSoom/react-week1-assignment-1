914100046d23efca632db595347b3fef9cd00b49
eonsang <2eonhworking@gmail.com>
2020-06-03 05:03:40 +0900

    리팩토링

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
-
 const createElement = (tagName, props, ...children) => {
   const element = document.createElement(tagName);
   Object.entries(props || {}).forEach(([key, value]) => {
@@ -16,33 +15,24 @@ const createElement = (tagName, props, ...children) => {
   return element;
 };
 
-function handleClick(value) {
-  value += 1;
-  render(value);
-}
-function handleClickNumber(value) {
-  render(value);
-}
-function render(count = 0) {
-  const element = (
+(function render(count = 0) {
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(
     <div id="wrapper" className="greeting hello">
       123123
       <p className="description">Hello, world!</p>
-      <button type="button" onClick={() => handleClick(count)}>
-        Click me! ({count})
+      <button type="button" onClick={() => render((count += 1))}>
+        Click me! (
+        {count}
+        )
       </button>
       <div>
         {[1, 2, 3].map((i) => (
-          <button type="button" onClick={() => handleClickNumber(i)}>
+          <button type="button" onClick={() => render(i)}>
             {i}
           </button>
         ))}
       </div>
-    </div>
+    </div>,
   );
-
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
-}
-
-render();
+}());
