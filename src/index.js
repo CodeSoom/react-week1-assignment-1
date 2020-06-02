cbbad731d5ccaf87eb9c842be826d669f5e37065
eonsang <2eonhworking@gmail.com>
2020-06-03 04:26:32 +0900

    createElement 추상화 처리 완료함

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
-
 /* @jsx createElement */
+
+const createElement = (tagName, ...children) => {
+  const element = document.createElement(tagName);
+  console.log(children);
+  console.log('--------------------------');
+  children.forEach((child) => {
+    console.log(child);
+    element.appendChild(child);
+  });
+  console.log('--------------------------');
+  console.log(element);
+  return element;
+};
+
+document
+  .getElementById('app')
+  .appendChild(
+    createElement(
+      'div',
+      ...[1, 2, 3].map((i) => document.createTextNode(`Hello, world!${i} `)),
+      createElement('p', document.createTextNode('Hello, world!!!!\n'), document.createTextNode('Hi!!! \n')),
+      createElement('p', document.createTextNode('Hi!!!!!!!!!!!!\n'), document.createTextNode('Hi22222222!!! \n')),
+    ),
+  );
