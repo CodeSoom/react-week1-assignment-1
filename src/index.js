04ee0f25bb8e931370c6bac394c0dd1573bfde48
sogoagain <imyong0@gmail.com>
2020-06-03 21:53:33 +0900

    MVVM 패턴 적용
    기존 코드에서는 view에 등록된 이벤트 리스너가 view를 직접  변경하는 전역함수를 호출하고,
    view는 그 이벤트 리스너를 등록하고 있었기에 의존 관계가 깊다고 판단되어 그 사이에 ViewModel을 추가 하였다.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 
 /* @jsx createElement */
+const { log } = console;
+
+// eslint-disable-next-line no-unused-vars
 const createElement = (tagName, props, ...children) => {
   const element = document.createElement(tagName);
   const propEntries = Object.entries(props || {});
@@ -11,57 +14,130 @@ const createElement = (tagName, props, ...children) => {
     return document.createTextNode(child);
   });
 
-  propEntries.forEach(([key, value]) => {
+  [...propEntries].forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
   });
   [...childNodes].forEach((node) => element.appendChild(node));
   return element;
 };
 
-let count = 0;
+const ViewModel = () => {
+  const TAG = '[ViewModel]';
+  const observers = new Set();
 
-const increaseCount = () => {
-  count += 1;
+  return {
+    addObserver(observer) {
+      log(TAG, 'addObserver()');
+      observers.add(observer);
+    },
+    notifyObservers(value) {
+      log(TAG, 'notifyObservers()');
+      observers.forEach((observer) => observer.onNotify(value));
+    },
+  };
 };
 
-const setCount = (newCount) => {
-  count = newCount;
-}
+const View = () => {
+  const TAG = '[View]';
 
-const onClickMe = () => {
-  increaseCount();
-  render();
+  return {
+    onNotify() {
+      log(TAG, 'onNotify()');
+    },
+  };
 };
 
-const onClickNumber = (count) => {
-  setCount(count);
-  render();
+const Counter = () => {
+  const TAG = '[Counter]';
+  const value = {
+    count: 0,
+  };
+
+  return {
+    increase() {
+      log(TAG, 'incrase()');
+      value.count += 1;
+    },
+    setCount(newCount) {
+      log(TAG, 'setCount()');
+      value.count = newCount;
+    },
+    getCount() {
+      log(TAG, 'getCount()');
+      return value.count;
+    },
+  };
 };
 
-const render = () => {
-  const element = (
-    <div id="hello-world" className="greeting">
-      <p>Hello World!</p>
-      <div>
-        <button type="button" onClick={() => onClickMe()}>
-          Click me! (
-          {count}
-          )
-        </button>
-      </div>
-      <div>
-        {[1, 2, 3].map((i) => (
-          <button type="button" onClick={() => onClickNumber(i)}>
-            {i}
+const CounterViewModel = (model) => {
+  const TAG = '[CounterViewModel]';
+  const viewModel = ViewModel();
+  const counter = model;
+
+  const notifyObservers = () => {
+    const count = counter.getCount();
+    viewModel.notifyObservers(count);
+  };
+
+  return {
+    ...viewModel,
+    onClickMe() {
+      log(TAG, 'onClickMe()');
+      counter.increase();
+      notifyObservers();
+    },
+    onClickNumber(number) {
+      log(TAG, 'onClickNumber()');
+      counter.setCount(number);
+      notifyObservers();
+    },
+    init() {
+      notifyObservers();
+    },
+  };
+};
+
+const CounterView = (viewModel) => {
+  const TAG = '[CounterView]';
+
+  const render = (count, onClickMe, onClickNumber) => {
+    const element = (
+      <div id="hello-world" className="greeting">
+        <p>Hello World!</p>
+        <div>
+          <button type="button" onClick={onClickMe}>
+            Click me! (
+            {count}
+            )
           </button>
-        ))}
+        </div>
+        <div>
+          {[1, 2, 3].map((i) => (
+            <button type="button" onClick={() => onClickNumber(i)}>
+              {i}
+            </button>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
 
-  const container = document.getElementById('app');
-  container.textContent = '';
-  container.appendChild(element);
+    const container = document.getElementById('app');
+    container.textContent = '';
+    container.appendChild(element);
+  };
+
+  return {
+    ...View(),
+    onNotify(value) {
+      log(TAG, 'onNotify()');
+      render(value, viewModel.onClickMe, viewModel.onClickNumber);
+    },
+  };
 };
 
-render();
+const counter = Counter();
+const counterViewModel = CounterViewModel(counter);
+const counterView = CounterView(counterViewModel);
+
+counterViewModel.addObserver(counterView);
+counterViewModel.init();
