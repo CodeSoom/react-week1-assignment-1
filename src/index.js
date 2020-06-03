b658aa968c4000b9b40d5893fd162e78e1ad7b7f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-03 11:50:52 +0900

    feat(index.js): createElement function
    - tagName, props, 자식 노드 정보를 입력 받아서 element를 생성하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  children.forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const element = (
+  <div>
+    <div>hello</div>
+  </div>
+);
+
+document.getElementById('app').appendChild(element);
