bae393d4e4fc76e87dbff8993a2677748b455e49
aumilkyeong <aumilkyeong@gmail.com>
2020-06-04 17:04:47 +0900

    assignment 1-1 tryout#1
    added argument and parameter to render() and some ESLint config.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,57 @@
-/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension, no-use-before-define */
 /* @jsx createElement */
+
+
+function createElement(tagName, props, ...children) { // eslint-disable-line no-unused-vars
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function handleClickNumber(value) {
+  render(value);
+}
+
+function handleClick(count) {
+  render(count += 1);
+}
+
+function render(count) {
+  const element = (
+    <div id="hello" className="greetings">
+      <p>Hello, World!!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          Click Me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            { i }
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render(0);
