27d5abd4e65895963ee1bbfe71c4445f173adf8a
Joylish <yeonzzu2@gmail.com>
2020-06-04 15:38:34 +0900

    create function 'createElement'

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+  children.forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+const element = (
+  <div>
+    <p>Hello, world</p>
+  </div>
+);
+document.getElementById('app').appendChild(element);
