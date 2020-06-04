2984cd60c7aba088929fa8dafd4e229a9385a194
Joylish <yeonzzu2@gmail.com>
2020-06-04 15:44:50 +0900

    add entries fucntion to get props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@
 /* @jsx createElement */
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
+  /*
+  entries 함수
+  {
+    id:1,
+    className: 2,
+  }
+  => [['id',1], ['className', 2]]
+  */
+  Object.entries(props || {}).forEach(([key, value]) => {
+    // props는 객체
+    // obj => const {key, value} = array;
+    // 매개변수에서 destructuring
+    element[key] = value;
+  });
+
   children.forEach((child) => {
     if (child instanceof Node) {
       element.appendChild(child);
@@ -9,10 +24,11 @@ function createElement(tagName, props, ...children) {
     }
     element.appendChild(document.createTextNode(child));
   });
+
   return element;
 }
 const element = (
-  <div>
+  <div id="hello" className="greeting">
     <p>Hello, world</p>
   </div>
 );
