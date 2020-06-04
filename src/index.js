33279d411c73405e3df7ce7ab01d343af10f55bd
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-04 00:42:07 -0700

    fix: remove let or var about count variable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 
 /* @jsx createElement */
+
+// MDN Destructuring assignment - Assignment without declaration
+({ count } = { count: 0 });
+
+function createElement(tags, props, ...children) {
+  const element = document.createElement(tags);
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+
+function handleClick() {
+  count = count + 1;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          { count }
+          )
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
