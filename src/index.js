be460f0d772f174176de254c591008013984bff2
Hwang <iamhwang9@gmail.com>
2020-06-05 14:39:03 +0900

    Test commit for week1-assignment-1 index.js let 제거 elint error 4 개선 필요

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
 
-function createElement(tagName, props, ...children) {    
-    const element = document.createElement(tagName);
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key,value]) =>{
-        element[key.toLowerCase()] = value;
-    });
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    children.flat().forEach(child => {
-        if(child instanceof Node){
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
 
-    return element;
+  return element;
 }
 
 
 function handleClick(value) {
-    value+=1;
-    console.log('click : '+value);
-    render(value);
+  value += 1;
+  render(value);
 }
 
-function handleClickNumber(value){
-    render(value);
+function handleClickNumber(value) {
+  render(value);
 }
 
 
-function render(count){
-    
-    const element = (
-        <div id="hello" className="greeting">
-            <p>Hello,world!</p>
-            <p>
-                <button type="button" onClick={ () => handleClick(count)}>
-                    click me!  ({count})                    
-                </button>
-            </p>
-            <p>
-                {[1,2,3].map( i => (
-                    <button type="button" onClick={() =>handleClickNumber(i)}>{i}</button>
-                ))}
-            </p>
-        </div>
-    )
+function render(count) {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello,world!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          click me!  (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))}
+      </p>
+    </div>
+  );
 
-    document.getElementById('app').textContent = '';    
-    document.getElementById('app').appendChild(element);
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
 render(0);
