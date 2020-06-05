ef9e7f83052e9a00694ba7b34b111ce350aa80d5
Paul An <anpaul0615@gmail.com>
2020-06-06 08:42:52 +0900

    "과제 1 - let 을 제거해보자" 최초 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
-
 /* @jsx createElement */
+
+// eslint-disable-next-line no-unused-vars
+function createElement(tagName, props = {}, ...children) {
+  const element = document.createElement(tagName);
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+
+function render(count) {
+  if (!Number.isInteger(count)) {
+    return;
+  }
+  const handleClick = (number) => {
+    render(number);
+  };
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count + 1)}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClick(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render(0);
