9d63a6fa2f4441e90b87b36c73240eb471e1f800
Dokyun <idk255@naver.com>
2020-09-14 22:42:57 +0900

    Update src/index.js - createElement()

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ let count = 0;
 
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
-  
-  Object.entries(props).forEach(([key, value]) => {
-    element[key] = value;
-  })
-}
 
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key] = value;
+  });
+
+  children.forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+}
 
 function handleClick() {
   count = count + 1;
