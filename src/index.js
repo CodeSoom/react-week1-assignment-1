1e41f32318ad7d046e620a0f9bd2f75e97b77085
koremp <im.koremp@gmail.com>
2020-09-14 22:06:55 +0900

    Update src/index.js - render function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+let count = 0;
+
+function handleClick() {
+  count = count + 1;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <p>{i}</p>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
