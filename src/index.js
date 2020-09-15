73f1ce934003cea1a6abd86db5162b79701172fe
oceanide <oceanide@gmail.com>
2020-09-15 11:24:54 +0900

    Use count object to remove let keyword
    This commit is included:
    - define a new count object to remove let keyword
    - define eslint rule to disable "no-use-before-define"

--- src/index.js
+++ src/index.js
@@ -1,2 +1,49 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+/* eslint no-use-before-define: ["error", { "functions": false}] */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const count = {
+  value: 0,
+};
+
+function handleClick() {
+  count.value += 1;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, oceanide!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click Me!
+          (
+          {count.value}
+          )
+        </button>
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
