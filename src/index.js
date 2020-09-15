155591a2861ec35e8db04b7f21a3758baee9a202
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-15 16:41:45 +0900

    let 변수를 제거하기
    let을 const변수로 변경하는 대신 오브젝트로 만들어서 value값을 받아서
    사용할 수 있게 했습니다.
    
    그런데 eslint를 돌릴때마다 전체적으로 모든 줄에서Expected linebreaks to
    be 'LF' but found 'CRLF'.eslintlinebreak-style) 오류가 납니다. 문제가
    없어보이는데 이렇게 오류가 떠서 이 오류는 어떻게 해결해야하는지
    궁금합니다.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+//
+
+const number = {
+  count: 0,
+};
+function handleClick() {
+  number.count += 1;
+  render();
+}
+
+function handleClickNumber(value) {
+  number.count = value;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" classNAme="greeting">
+      <p>Hello, world</p>
+      <p>
+        <button onClick={handleClick}>
+          Click me! (
+          {number.count}
+          )
+          {' '}
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => <button onClick={() => handleClickNumber(i)}>{i}</button>)}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
