ed9d33b95484871fe2e5b02dcf5ae15e52ec84d2
wowgod9504 <mike1124@naver.com>
2020-09-16 15:03:50 +0900

    í¾¨ Modify code after received a code review
    After I received a code review, I followed the eslint rules and changed
    the code to make it easier to understand.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-/* eslint-disable no-use-before-define */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+
 /* @jsx createElement */
 
-function handleClick(countNumber) {
-  const plusCount = countNumber + 1;
-  render(plusCount);
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
 }
 
-function handleClickNumber(value) {
-  render(value);
-}
+function render(count = 0) {
+  const handleClick = () => render(count + 1);
+  const handleClickNumber = (i) => render(i);
 
-function render(plusCount = 0) {
-  const CountNumber = plusCount;
   const element = (
     <div id="hello" className="greeting">
       <p>Hello, world!</p>
       <p>
-        <button type="button" onClick={() => handleClick(CountNumber)}>
+        <button type="button" onClick={() => handleClick()}>
           Click me! (
-          {CountNumber}
+          {count}
           )
         </button>
       </p>
@@ -33,21 +44,6 @@ function render(plusCount = 0) {
 
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(element);
-}
-
-function createElement(tagName, props, ...children) {
-  const element = document.createElement(tagName);
-  Object.entries(props || {}).forEach(([key, value]) => {
-    element[key.toLowerCase()] = value;
-  });
-  children.flat().forEach((child) => {
-    if (child instanceof Node) {
-      element.appendChild(child);
-      return;
-    }
-    element.appendChild(document.createTextNode(child));
-  });
-  return element;
 }
 
 render();
