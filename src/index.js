1a59d20c9d56631654c7c5fa924358dfa51848b5
wjy5446 <wjy5446@gmail.com>
2020-09-16 16:29:39 +0900

    feat) 베이스 코드 작성 (let 사용)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+    return element;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+    } else {
+      element.appendChild(document.createTextNode(child));
+    }
+  });
+
+  return element;
+}
+
+let count = 0;
+
+function handleClick() {
+  count += 1;
+  render();
+}
+
+function handleClickNumber(i) {
+  count = i;
+  render();
+}
+
+function render() {
+  const element = (
+    <div>
+      <p>Hello, world {count}</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          click me!
+          </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById("app").textContent = "";
+  document.getElementById("app").appendChild(element);
+}
+
+render();
+
