1cd1040b40d8826b56262767457c052db316843b
ParkDongJo <pdj10211@gmail.com>
2020-09-17 00:57:15 +0900

    [week1-1] 강의따라하기 1차 구현

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+
+const createElement = (tagName, ...children) => {
+    const element = document.createElement(tagName);
+
+    children.forEach((child) => {
+        element.appendChild(child);
+    });
+
+    return element;
+}
+
+
+const root = createElement(
+    'div',
+    createElement(
+        'p',
+        ...[1, 2, 3].map(i => (
+            document.createTextNode(`Hello, world! ${i} | `)
+        ))
+    ),
+    createElement(
+        'p',
+        document.createTextNode('Hi, dongjo!'),
+        document.createTextNode('Hi, dongjo!!')
+    ),
+);
+document.getElementById('app').appendChild(root)
