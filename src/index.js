16fe8d66a750c7e1c9c0de6831c7d65f073f8740
ParkDongJo <pdj10211@gmail.com>
2020-09-17 01:28:53 +0900

    [week1-1] 강의따라하기 2차 구현

--- src/index.js
+++ src/index.js
@@ -2,29 +2,58 @@
 /* @jsx createElement */
 
 
-const createElement = (tagName, ...children) => {
+const createElement = (tagName, props, ...children) => {
     const element = document.createElement(tagName);
 
-    children.forEach((child) => {
-        element.appendChild(child);
+    Object.entries(props || {}).forEach(([key, value]) => {
+        element[key.toLowerCase()] = value
+    })
+
+    children.flat().forEach((child) => {
+        if (child instanceof Node) {
+            element.appendChild(child);
+            return;
+        }
+        element.appendChild(document.createTextNode(child));
     });
 
     return element;
 }
 
+let count = 0;
 
-const root = createElement(
-    'div',
-    createElement(
-        'p',
-        ...[1, 2, 3].map(i => (
-            document.createTextNode(`Hello, world! ${i} | `)
-        ))
-    ),
-    createElement(
-        'p',
-        document.createTextNode('Hi, dongjo!'),
-        document.createTextNode('Hi, dongjo!!')
-    ),
-);
-document.getElementById('app').appendChild(root)
+const handleClick = () => {
+    count += 1;
+    render();
+}
+
+const handleClickNumber = (value) => {
+    count = value;
+    render();
+}
+
+const render = () => {
+    const element = (
+        <div id="hello" className="greeting">
+            <p>Hello, world!</p>
+            <p>
+                <button type="button" onClick={handleClick}>
+                    Click me!
+                    ({ count })
+                </button>
+            </p>
+            <p>
+                {[1, 2, 3].map((i) => (
+                    <button type="button" onClick={() => handleClickNumber(i)}>
+                        {i}
+                    </button>
+                ))}
+            </p>
+        </div>
+    );
+
+    document.getElementById('app').textContent = '';
+    document.getElementById('app').appendChild(element);
+}
+
+render();
