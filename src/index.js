aef044c3c4d5d7046c55d064020560a393e2c475
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-17 23:53:48 +0900

    Implement a count program without let
    I implemented the count program as a week 1 project that does not use'let'.
    It was implemented in accordance with the conditions to pass the written e2e test.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,49 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagname, props, ...children) {
+  const element = document.createElement(tagname);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const counting = (num) => num + 1;
+
+function render(count = 0) {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello,world!</p>
+      <p>
+        <button type="button" onClick={() => render(counting(count))}>
+          Click me!
+        </button>
+
+        <button type="button" onClick={() => render(2)}>
+          (2)
+        </button>
+      </p>
+
+      <p>
+        (
+        {count}
+        )
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,7 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    port: 9000,
+  },
 };
