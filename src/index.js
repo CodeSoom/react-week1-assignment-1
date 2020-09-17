9dd2e5a349fefa6b6df289aafdd69b9c2040af11
ParkDongJo <pdj10211@gmail.com>
2020-09-17 12:52:19 +0900

    [week1-3] lint, test 통과하기
    * render() 내부에 click event 함수들 옮김
    * parameter에 값 재할당하는 코드를 제거함
    
    lint, test 코드 통과를 위해 위와같은 작업들을 했습니다

--- src/index.js
+++ src/index.js
@@ -1,57 +1,58 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
 
-const INITIAL_COUNT = 0
-
 const createElement = (tagName, props, ...children) => {
-    const element = document.createElement(tagName);
+  const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase()] = value
-    })
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    children.flat().forEach((child) => {
-        if (child instanceof Node) {
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
 
-    return element;
-}
+  return element;
+};
 
-const handleClick = (count) => {
-    count += 1;
-    render(count);
-}
+function render(pCount = 0) {
+  let count = pCount;
 
-const handleClickNumber = (value) => {
+  const handleClickNumber = (value) => {
     render(value);
+  };
+
+  const handleClick = (value) => {
+    render(value + 1);
+  };
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => { count = handleClick(pCount); }}>
+          Click me!
+          (
+          { count }
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
-const render = (count = 0) => {
-    const element = (
-        <div id="hello" className="greeting">
-            <p>Hello, world!</p>
-            <p>
-                <button type="button" onClick={() => { count = handleClick(count) }}>
-                    Click me!
-                    ({ count })
-                </button>
-            </p>
-            <p>
-                {[1, 2, 3].map((i) => (
-                    <button type="button" onClick={() => handleClickNumber(i)}>
-                        {i}
-                    </button>
-                ))}
-            </p>
-        </div>
-    );
-
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
-}
-
-render(INITIAL_COUNT);
+render();
