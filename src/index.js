c7bb3148331ec574983033e6ff23d0a0d6d0316b
shinhyogeun <tls1gy2rms3@naver.com>
2021-01-19 00:14:55 +0900

    refactor : render 함수에 default value 적용

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function createElement(tagName, props, ...children) {
   return paragraph;
 }
 
-function render(count) {
+function render(count = 0) {
   document.getElementById('app').textContent = '';
   document.getElementById('app').appendChild(
     <div id="hello" className="greeting">
@@ -36,4 +36,4 @@ function render(count) {
   );
 }
 
-render(0);
+render();
