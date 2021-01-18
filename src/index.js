ecbf1edab7f291642540068eb71076f73ca1317e
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-19 06:58:58 +0900

    Add count and don't use let
    let사용을 피하기 위해 array의 요소들을 더하는 reduce를 통해 count를
    증가시키도록 구현했습니다.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children){
+    const element = document.createElement(tagName);   
+    Object.entries(props || {}).forEach(([key, value]) => {
+        element[key.toLowerCase()] = value;
+    })
+    children.flat().forEach((child) => {
+        if (child instanceof Node) {
+            element.appendChild(child);
+            return;
+        }
+        element.appendChild(document.createTextNode(child));
+    });
+    return element;
+}
+
+
+const count = [0];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+function handleClick(count){
+    console.log('click!');
+    count.push(1);
+    count = count.reduce(reducer);
+    console.log(count);
+    render();
+}
+function handleClickNumber(value) {
+    render();
+}
+function render(){
+const element = (
+    <div id="hello" className="greeting">
+ <p>Hello, world!</p>
+ <p>
+     <button type="button" onClick={()=>handleClick(count)}>
+        Click me! 
+        (
+            
+        {count.reduce(reducer)}
+        )
+     </button>
+ </p>
+ <p>
+     {[1,2,3].map((i)=>(
+         <button type="button" onClick={()=>handleClickNumber(i)}>
+             {i}
+         </button>
+     ))}
+ </p>
+    </div>
+   
+);
+document.getElementById('app').textContent = '';
+document.getElementById('app').appendChild(element);
+}
+
+render();
