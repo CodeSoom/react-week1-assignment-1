8b09ec8cb8d269ea1ee12aa55c5402fd16a48bbd
shinhyogeun <tls1gy2rms3@naver.com>
2021-01-18 20:59:50 +0900

    refactor : let 제거로 변수 재할당 제거

--- src/index.js
+++ src/index.js
@@ -1,2 +1,53 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const paragraph = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach((item) => {
+    const [key, value] = item;
+    paragraph[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((item) => {
+    if (item instanceof Node) {
+      paragraph.appendChild(item);
+      return;
+    }
+    paragraph.appendChild(document.createTextNode(item));
+  });
+
+  return paragraph;
+}
+
+const count = {
+  number: 0,
+};
+
+function handleClick() {
+  count.number += 1;
+  render();
+}
+
+function handleButtonClick(item) {
+  count.number = item;
+  render();
+}
+
+function render() {
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(
+    <div id="hello" className="greeting">
+      <button type="button" onClick={handleClick}>
+        Click me!
+        (
+        {count.number}
+        )
+      </button>
+      <p>
+        {[1, 2, 3].map((item) => <button type="button" onClick={() => handleButtonClick(item)}>{item}</button>)}
+      </p>
+    </div>,
+  );
+}
+
+render();
