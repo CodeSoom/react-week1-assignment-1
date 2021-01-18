fad00936edc9e91e1e0caa86b26a34b283ede503
EL <ickyicky@gmail.com>
2021-01-18 21:20:34 +0900

    FEAT: Adds a index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function handleClickNumber(value) {
+  render(value);
+}
+function handleClick(countValue) {
+  render(countValue + 1);
+}
+
+function render(countValue) {
+  const element = (
+    <div>
+      <p id="hello" className="greeting">
+        Hello, world!
+      </p>
+      <p>
+        <button type="button" onClick={() => handleClick(countValue)}>
+          Click me!
+          {' '}
+          <span id="countValue">{countValue}</span>
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))}
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+render(0);
