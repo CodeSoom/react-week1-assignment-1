83ac13c2daae3223c9baf5543dadca85c8478e4a
gimsesu <tkfrn4799@gmail.com>
2021-01-20 00:20:42 +0900

    Remove let
    - gimsesu 브랜치가 CodeSoom(upstream) repo에 이미 존재하여
    'gimsesu3'이라는 브랜치를 생성하여 커밋
    - localstorage를 사용하여 count 값을 저장하고 출력했습니다.
    - esling 룰을 별도로 추가하지 않았습니다. --fix 옵션을 사용하니 대부분
    해결되었습니다..

--- src/index.js
+++ src/index.js
@@ -1,2 +1,55 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(name, props, ...children) {
+  const elem = document.createElement(name);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    elem[key.toLowerCase()] = value;
+  });
+
+  children.forEach((child) => {
+    if (child instanceof Node) {
+      elem.appendChild(child);
+      return;
+    }
+    elem.appendChild(document.createTextNode(child));
+  });
+  return elem;
+}
+
+localStorage.setItem('count', '0');
+
+function handleClick() {
+  if (!localStorage.getItem('count')) {
+    localStorage.setItem('count', '0');
+  }
+  localStorage.setItem('count', `${parseInt(localStorage.getItem('count'), 10) + 1}`);
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick();
+            render();
+          }}
+        >
+          Click me!
+          (
+          {localStorage.getItem('count')}
+          )
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
