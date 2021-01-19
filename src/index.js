20746b0ef0ccb63458e8d8880d3b4c070944e7aa
canary4651 <canary4651@gmail.com>
2021-01-19 21:11:48 +0900

    Remove let
    Remove `let` and use `const`.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const count = { number: 0 };
+
+function render() {
+  const handleClick = () => {
+    count.number += 1;
+    render();
+  };
+
+  const handleClickNumber = (value) => {
+    count.number = value;
+    render();
+  };
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          {count.number}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
