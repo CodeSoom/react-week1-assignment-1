d3746c635809e1f2a1926d029072eab9b0bf219d
younghoon <secret9179@gmail.com>
2021-01-19 20:54:46 +0900

    마지막 한줄 수정

--- src/index.js
+++ src/index.js
@@ -2,59 +2,61 @@
 /* @jsx createElement */
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
-function createElement(tagName, props, ...children){
-    console.log(children);
-    console.log(tagName, props, children);
+function createElement(tagName, props, ...children) {
+  console.log(children);
+  console.log(tagName, props, children);
 
-    const element = document.createElement(tagName);
+  const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase()] = value;
-    })
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    children.flat().forEach((child) => {
-        if(child instanceof Node){
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
 
-    return element;
+  return element;
 }
 
 let count = 0;
 
-function handleClick(){
-    count += 1;
-    render();
+function handleClick() {
+  count += 1;
+  render();
 }
 
-function handleClickNumber(value){
-    count = value;
-    render();
+function handleClickNumber(value) {
+  count = value;
+  render();
 }
 
-function render(){
-    const element = (
-        <div id="hello" className="greeting">
-            <p>Hello, React!</p>
-            <p>
-                <button type="button" onClick={handleClick}>
-                    Click me! {count}
-                </button>
-            </p>
-            <p>
-                {[1, 2, 3].map((i) =>(
-                    <button type="button" onClick={()=> handleClickNumber(i)}>
-                        {i}
-                    </button>
-                ))}
-            </p>
-        </div>
-    );
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, React!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          {' '}
+          {count}
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
 render();
