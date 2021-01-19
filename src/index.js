266c0d0d86c3056c038e385dce1a15f88dd4fac0
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-01-19 14:36:30 +0900

    Feat: assignment-1
    - 객체안에 state, methods 선언하여 let 없이 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,2 +1,67 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+/* eslint-disable react/jsx-one-expression-per-line */
 /* @jsx createElement */
+const createElement = (tagName, props, ...children) => {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+};
+const countChange = {
+  state: {
+    count: 0,
+    setState(key, value) {
+      const oldState = countChange.state;
+      if (oldState[key] === value) {
+        return;
+      }
+
+      countChange.state[key] = value;
+      countChange.methods.render();
+    },
+  },
+  methods: {
+    handleClick() {
+      const newCount = countChange.state.count + 1;
+      countChange.state.setState('count', newCount);
+    },
+    handleClickNumber(value) {
+      countChange.state.setState('count', value);
+    },
+    render() {
+      const { count } = countChange.state;
+      const { handleClick, handleClickNumber } = countChange.methods;
+      const element = (
+        <div id="hello" className="gree">
+          <p>헬로월드</p>
+          <p>
+            <button type="button" onClick={handleClick}>
+              Click me! ({count})
+            </button>
+          </p>
+          <p>
+            {[1, 2, 3].map((i) => (
+              <button type="button" onClick={() => handleClickNumber(i)}>
+                {i}
+              </button>
+            ))}
+          </p>
+        </div>
+      );
+      document.getElementById('app').textContent = '';
+      document.getElementById('app').appendChild(element);
+    },
+  },
+};
+
+countChange.methods.render();
