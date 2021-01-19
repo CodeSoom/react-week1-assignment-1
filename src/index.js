72b275cfe83f6b443b6c8eb491e6f64a424d60d0
Yoon <yoonhe@gmail.com>
2021-01-19 13:31:40 +0900

    let을 하용하지 않고 기능 구현하기, 변수에 값을 재할당하면 안됨
    값을 재할당하지 않고 기능을 구현하기 위해 재귀함수의 매개변수를 이용해 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
-/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLocaleLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      return element.appendChild(child);
+    }
+    return element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function render(count = 0) {
+  const element = (
+    <div>
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => render(count + 1)}>
+          Click me! (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => render(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
