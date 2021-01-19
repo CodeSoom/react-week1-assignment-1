1628c00a160dca71aabd7f6da0ac17a6d0de6adf
Najeong Kim <atpeoe1023@gmail.com>
2021-01-19 21:53:06 +0900

    Count를 없애려고 매개변수로 넣으려 했으나 해결이 되지 않았다

--- src/index.js
+++ src/index.js
@@ -1,2 +1,58 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach(child => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+//let count = 0;
+
+function handleClick(count) {
+  //console.log(count);
+  render();
+}
+
+function handleClickNumber(value) {
+  handleClickNumber(value) = handleClick(value);
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          Click me! ({count++})
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map(i => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+
+};
+
+render();
