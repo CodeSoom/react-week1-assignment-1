7dc6889d0fa93337013ddd134b19a3ffbf0937d3
ErickRyu <rkggmdii@gmail.com>
2021-01-20 23:08:00 +0900

    Update the number on the button without a variable
    Instead of using the count global variable, I manipulated DOM
    object to update the number.

--- src/index.js
+++ src/index.js
@@ -4,44 +4,62 @@
 function createElement(tagName, props, ...children) {
   const element = document.createElement(tagName);
 
-  children.flat().forEach(child => {
-    if (child instanceof Node){
-      element.appendChild(child)
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
       return;
     }
-    element.appendChild(document.createTextNode(child))
-  })
+    element.appendChild(document.createTextNode(child));
+  });
 
   Object.entries(props || {}).forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
-  })
+  });
 
   return element;
 }
 
-let count = 0;
-
-function handleClick(){
-  count += 1;
-  console.log(count)
-  render()
+function getInnerTextById(id) {
+  return document.getElementById(id).innerText;
 }
 
-function render(){
-  const element = ( <div id='name' className="hi"> 
-      <p> Hello </p> 
-      <p> Hi </p> 
-      <button onClick={handleClick}>
+function setInnerTextById(id, replace) {
+  document.getElementById(id).innerText = replace;
+}
+
+function handleClick() {
+  const innerText = getInnerTextById('count-click-btn');
+
+  const replaced = innerText.replace(/[0-9]+/g, (match) => parseInt(match, 10) + 1);
+
+  setInnerTextById('count-click-btn', replaced);
+}
+
+function handleClickNumber(value) {
+  const innerText = getInnerTextById('count-click-btn');
+
+  const replaced = innerText.replace(/[0-9]+/g, () => value);
+  setInnerTextById('count-click-btn', replaced);
+}
+
+const element = (
+  <div id="hello" className="greeting">
+    <p>Hello, world!</p>
+    <p>
+      <button id="count-click-btn" type="button" onClick={handleClick}>
         Click me!
-        ({count})
+        (0)
+      </button>
+    </p>
+    <p>
+      {[1, 2, 3].map((i) => (
+        <button type="button" onClick={() => handleClickNumber(i)}>
+          {i}
         </button>
-        <p>
-        {[1,2, 3].map(i =>(<p>{i}</p>))}
-        </p>
-    </div>
-  )
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
-}
+      ))}
+    </p>
 
-render()
+  </div>
+);
+document.getElementById('app').textContent = '';
+document.getElementById('app').appendChild(element);
