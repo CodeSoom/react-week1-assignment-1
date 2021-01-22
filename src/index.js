6dd998d4f3cce0ed5957e2bf25a8ef12458cd1dd
ErickRyu <rkggmdii@gmail.com>
2021-01-22 21:39:59 +0900

    Use number only inside render()

--- src/index.js
+++ src/index.js
@@ -19,47 +19,33 @@ function createElement(tagName, props, ...children) {
   return element;
 }
 
-class NumberState {
-  constructor() {
-    this.state = { number: 0 };
+function render(number = 0) {
+  function setNumber(num) {
+    render(num);
   }
-
-  addNumber() {
-    this.state.number += 1;
-    this.render();
-  }
-
-  setNumber(num) {
-    this.state.number = num;
-    this.render();
-  }
-
-  render() {
-    const element = (
-      <div id="hello" className="greeting">
-        <p>Hello, world!</p>
-        <p>
-          <button id="count-click-btn" type="button" onClick={() => this.addNumber()}>
-            Click me!
-            (
-            {this.state.number}
-            )
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button id="count-click-btn" type="button" onClick={() => setNumber(number + 1)}>
+          Click me!
+          (
+          {number}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => setNumber(i)}>
+            {i}
           </button>
-        </p>
-        <p>
-          {[1, 2, 3].map((i) => (
-            <button type="button" onClick={() => this.setNumber(i)}>
-              {i}
-            </button>
-          ))}
-        </p>
+        ))}
+      </p>
 
-      </div>
-    );
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
-  }
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
-const numberState = new NumberState();
-numberState.render();
+render();
