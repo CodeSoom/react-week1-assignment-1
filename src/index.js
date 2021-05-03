6f75733abccb556e8cc5a2c281cc91521efebf63
ParkSeJun <fgfg1202@gmail.com>
2021-05-04 01:54:08 +0900

    npx and npm not working
    npx and npm not working
    
    google search fail
    only linux result has been found

--- src/index.js
+++ src/index.js
@@ -1,2 +1,61 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props = {}, ...children ) {
+    const element = document.createElement(tagName);
+
+    Object.entries(props || {}).forEach(([key, value]) => {
+        element[key.toLowerCase()] = value;
+    });
+    
+    children.flat().forEach((child) => {
+        if (child instanceof Node) {
+          element.appendChild(child);
+          return;
+        }
+        element.appendChild(document.createTextNode(child));
+      });
+    
+      return element;
+    };
+
+function handleClick_countValue(value) {
+    value = value +1;
+    document.getElementById("button").value(value);
+    document.getElementById("button").innerHTML(`click Me ${value}`);
+    render();
+}
+
+function handler_Click(value) {
+    document.getElementById("button").value(value);
+    document.getElementById("button").innerHTML(`click Me ${value}`);
+
+    render();
+}
+
+function render(){
+    const element = (
+        <div id = "hello" className = "greeting">
+            <p>hello World</p>
+            <p>
+                <button id = "button" type = "button" 
+                onClick = {() => handleClick_countValue(document.getElementById("button").value())} value = "0">
+                    click Me 0
+                </button>
+            </p>
+            <p>
+                {[1, 2, 3].map((i) =>
+                <button type = "button" onClick = {() => handler_Click(i)}>
+                    {i}
+                </button>
+                )}
+            </p>
+        </div>
+    );
+
+    document.getElementById("app").textContent ="";
+    document.getElementById("app").appendChild(element);
+};
+
+render();
+%s/^M$//g
+
