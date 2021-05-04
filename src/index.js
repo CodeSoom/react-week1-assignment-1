b37e83dc4cafdd24691eecd0691645d9b7bb8784
hanvit <hanvit7@gmail.com>
2021-05-05 00:50:48 +0900

    Remove let
    let을 사용하지 않기 위해 count 변수 선언을 const 배열로 변경
    no-use-before-define을 피하기 위해 handleClick함수와
    handleClickNumber 함수를 익명함수로 변경

--- src/index.js
+++ src/index.js
@@ -1,2 +1,69 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  // console.log(tagName, props, ...children);
+
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const count = [0];
+
+function handleClick() {
+  count[0] += 1;
+}
+
+function handleClickNumber(value) {
+  count[0] = value;
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, wordl!!</p>
+      <p>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick();
+            render();
+          }}
+        >
+          Click me!
+          {' '}
+          {`(${count[0]})`}
+        </button>
+      </p>
+      {[1, 2, 3].map((i) => (
+        <button
+          type="button"
+          onClick={() => {
+            handleClickNumber(i);
+            render();
+          }}
+        >
+          {i}
+        </button>
+      ))}
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
