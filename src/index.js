8722dbdfcb58170acc25bd203807c11ffeeab835
최한울 <hanul.choi95@nts-corp.com>
2021-05-04 22:39:06 +0900

    [1주차] let을 제거해보자
    - 객체 프로퍼티는 변경이 가능하다는 사실을 이용해 해결
    - lint를 돌려보며 코드 수정. 생각한 것보다 lint로 알게된 규칙이 많음.
    - 인수테스트 통과

--- src/index.js
+++ src/index.js
@@ -2,60 +2,57 @@
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
-    console.log(children);
+  const element = document.createElement(tagName);
 
-    const element = document.createElement(tagName);
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    Object.entries(props || {}).forEach(([key, value]) => (
-        element[key.toLowerCase()] = value
-    ));
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
 
-    children.flat().forEach(child => {
-        if (child instanceof Node) {
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child))
-    });
-
-    return element;
+  return element;
 }
 
-const count = {number: 0};
-
-function handleClick() {
-    count.number += 1;
-    render();
-}
-
-function handleClickNumber(value) {
-    count.number = value;
-    render();
-}
+const count = { number: 0 };
 
 function render() {
-    const element = (
-        <div id="hello" className="greeting">
-            <p>Hello, world!!</p>
-            <p>
-                <button type="button" onClick={handleClick}>
-                    Click me!
-                    (
-                        {count.number}
-                    )
-                 </button>
-            </p>
-            <p>
-                {[1, 2, 3].map(i => (
-                    <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
-                ))}
-            </p>
-        </div>
-    )
+  const handleClick = () => {
+    count.number += 1;
+    render();
+  };
 
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
+  const handleClickNumber = (value) => {
+    count.number = value;
+    render();
+  };
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          {count.number}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
 render();
-
