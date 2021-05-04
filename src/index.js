e1d3b0fff172e40ca7965ffbe33a3e924ffe23dc
Parksejun <fgfg1202@gmail.com>
2021-05-04 12:51:47 +0900

    git very hard

--- src/index.js
+++ src/index.js
@@ -1,61 +1,62 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
-function createElement(tagName, props = {}, ...children ) {
-    const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase()] = value;
-    });
-    
-    children.flat().forEach((child) => {
-        if (child instanceof Node) {
-          element.appendChild(child);
-          return;
-        }
-        element.appendChild(document.createTextNode(child));
-      });
-    
-      return element;
-    };
+function createElement(tagName, props = {}, ...children) {
+  const element = document.createElement(tagName);
 
-function handleClick_countValue(value) {
-    value = value +1;
-    document.getElementById("button").value(value);
-    document.getElementById("button").innerHTML(`click Me ${value}`);
-    render();
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
 }
 
-function handler_Click(value) {
-    document.getElementById("button").value(value);
-    document.getElementById("button").innerHTML(`click Me ${value}`);
-
-    render();
+function clickCount(value) {
+  const value2 = parseInt(value, 10) + 1;
+  document.getElementById('button').innerText = `click Me ${value2}`;
+  document.getElementById('button').value = value2;
 }
 
-function render(){
-    const element = (
-        <div id = "hello" className = "greeting">
-            <p>hello World</p>
-            <p>
-                <button id = "button" type = "button" 
-                onClick = {() => handleClick_countValue(document.getElementById("button").value())} value = "0">
-                    click Me 0
-                </button>
-            </p>
-            <p>
-                {[1, 2, 3].map((i) =>
-                <button type = "button" onClick = {() => handler_Click(i)}>
-                    {i}
-                </button>
-                )}
-            </p>
-        </div>
-    );
+function handlerClick(valueI) {
+  document.getElementById('button').innerText = `click Me ${valueI}`;
+  document.getElementById('button').value = valueI;
+}
 
-    document.getElementById("app").textContent ="";
-    document.getElementById("app").appendChild(element);
-};
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>hello World</p>
+      <p>
+        <button
+          id="button"
+          type="button"
+          value="0"
+          onClick={() => clickCount(document.getElementById('button').value)}
+        >
+          click Me 0
+        </button>
+        {/* <input style={{ display: 'none' }} id="dataSaver" value="0" /> */}
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handlerClick(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  //   document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
 
 render();
-%s/^M$//g
-
