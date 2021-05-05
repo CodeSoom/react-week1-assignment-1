277bc841554b806fb14d82aa75ecc38e5720a890
seong-dodo <dyrnxm52@hanmail.net>
2021-05-06 00:07:13 +0900

    과제1 - 피드백 반영
    const제거 / 함수 매개변수 사용 / eslint

--- src/index.js
+++ src/index.js
@@ -2,59 +2,47 @@
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
-    const element = document.createElement(tagName);
+  const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase()] = value;
-    });
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    children.flat().forEach((child) => {
-        if (child instanceof Node) {
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    })
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
 
-    return element;
+  return element;
 }
 
-const state = {count: 0};
+function render(count) {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world</p>
+      <p>
+        <button type="button" onClick={() => render(count + 1)}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => render(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
 
-function handleClick() {
-    state.count = state.count + 1;
-    render();
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
-function handleClickNumber(value) {
-    state.count = value;
-    render();
-}
-
-function render() {
-    const element = (
-        <div id="hello" className="greeting">
-            <p>Hello, world</p>
-            <p>
-              <button type="button" onClick={handleClick}>
-                Click me!
-                (
-                {state.count}
-                )
-              </button>
-            </p>
-            <p>
-                {[1, 2, 3].map((i) => (
-                    <button type="button" onClick={()=> handleClickNumber(i) }>
-                     {i}
-                    </button>
-                ))}
-            </p>
-        </div>
-    );
-
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
-}
-
-render();
+render(0);
