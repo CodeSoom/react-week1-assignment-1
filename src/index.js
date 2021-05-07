f2414211d10143b57f74c64899aae80fbd4cd61e
studioLuda <studio.luda.work@gmail.com>
2021-05-07 20:55:22 +0900

    과제 1-1(let 사용하지 않기) 제출 : 1차

--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
-let count = 0;
+
+const data = { count: 0 };
 
 function handleClick() {
-    count += 1;
-    console.log(`${count} times clicked`);
-    render();
+  console.log(data);
+  data.count += 1;
+  console.log(`${data.count} times clicked`);
+  render();
 }
 
 function handleClickNumber(value) {
-    count = value;
-    render();
+  data.count = value;
+  render();
 }
 function createElement(tagName, props, ...children) {
-  // console.log(tagName,props,...children)
+// console.log(tagName,props,...children)
 
-const element = document.createElement(tagName);
-Object.entries(props || {}).forEach(([key, value]) => {
+  const element = document.createElement(tagName);
+  Object.entries(props || {}).forEach(([key, value]) => {
     element[key.toLowerCase()] = value;
-});
-children.flat().forEach((child) => {
+  });
+  children.flat().forEach((child) => {
     if (child instanceof Node) {
-        element.appendChild(child);
-        return;
+      element.appendChild(child);
+      return;
     }
     element.appendChild(document.createTextNode(child));
-    });
-    return element;
+  });
+  return element;
 }
 function render() {
-
-const element = (
+  const element = (
     <div id="hello" className="greeting">
-    <p>bable test !</p>
-    <p>
-        <button type="button" onClick={handleClick} count={0}>
-        click me ! (
-        {count}
-        )
-        {' '}
+      <p>bable test !</p>
+      <p>
+        <button id="clickButton" type="button" onClick={handleClick} value={0}>
+          Click me! (
+          {data.count}
+          )
         </button>
-    </p>
-    <p>
+      </p>
+      <p>
         {
             [1, 2, 3].map((i) => (
-                <button type="button" onClick={() => { handleClickNumber(i); }}>{i}</button>
+              <button type="button" onClick={() => { handleClickNumber(i); }}>{i}</button>
             ))
         }
-    </p>
+      </p>
     </div>
-);
+  );
 
   document.getElementById('app').textContent = ''; // 랜더시 초기화 (빈값으로 초기화)
   document.getElementById('app').appendChild(element); // app 이라 선언된 공간에 컴포넌트 추가.
