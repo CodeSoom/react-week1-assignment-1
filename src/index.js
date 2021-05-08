b37768088c9061d07b19d05be089ae8a33e629b0
studioLuda <studio.luda.work@gmail.com>
2021-05-08 18:02:23 +0900

    1-1 과제 제출 2차 : 피드백 일부 반영
    반영된 사항
    
    1) onClick구현시 별도의 핸들러 함수 만들기
    2) 변수명에 명확한 의도 담기
    
    아직 해결하지 못하점
    
    count값을 객체로 관리하는 방법보다 더 나은 방법 찾기

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@
 // eslint console.log 사용금지에 대하여 : https://eslint.org/docs/rules/no-console.html
 // 원인 :  일반적으로 사용 console중인 통화 는 프로덕션으로 푸시되기 전에 제거되어야합니다.
 
-// .js 파일에서 eslint rull 추가하는 법
-
-// 1-1 과제 제출 1차
+// 1-1 과제 제출 2차
+/** 피드백 메모 & 생각정리 & 목표
+ *
+ * 1) count값을 객체로 관리한다고 해서 재할당이 불가능한 것은 아니다.
+ *    => count 값에 접근할 수 있는 자격을 가진자를 별도로 분리해야 할것 같다 : 그런데 아직 어떻게 해결해야 할지 잘 모르겠음
+ *
+ *
+ * 2) onClick 구현시 별도의 핸들러 함수를 만들어 사용하자 (완료)
+ *    =>  기존 handler함수 구현에서 render 호출시 'use-before-define' EeLint 오류가 있음
+ *    => handler 함수를 별도 구현 하면서, use-before-define 해결도 할 수 있는 방법을 찾아야함 : 함수안에 함수를 선언함.
+ *
+ * 3) 변수명에 명확하게 의도를 담을 것 (완료)
+ *    =>  resetVal 수정하기 : 수정 완료  
+ */
 const data = { count: 0 };
-
-// function handleClick() {
-//   console.log(data);
-//   data.count += 1;
-//   console.log(`${data.count} times clicked`);
-//   render();
-// }
 
 function createElement(tagName, props, ...children) {
   // console.log(tagName,props,...children)
@@ -36,6 +40,19 @@ function createElement(tagName, props, ...children) {
 }
 
 function render() {
+  function handleClickNumber(newValue) {
+    return () => {
+      data.count = newValue;
+      render();
+    };
+  }
+
+  function handleClick() {
+    data.count += 1;
+    console.log(`${data.count} times clicked`);
+    render(data.count);
+  }
+
   const element = (
     <div id="hello" className="greeting">
       <p>bable test !</p>
@@ -43,11 +60,7 @@ function render() {
         <button
           id="clickButton"
           type="button"
-          onClick={() => {
-            data.count += 1;
-            console.log(`${data.count} times clicked`);
-            render();
-          }}
+          onClick={handleClick}
           value={0}
         >
           Click me! (
@@ -57,15 +70,12 @@ function render() {
       </p>
       <p>
         {' '}
-        {[1, 2, 3].map((resetVal) => (
+        {[1, 2, 3].map((newValue) => (
           <button
             type="button"
-            onClick={() => {
-              data.count = resetVal;
-              render();
-            }}
+            onClick={handleClickNumber(newValue)}
           >
-            {resetVal}
+            {newValue}
           </button>
         ))}
       </p>
@@ -76,4 +86,4 @@ function render() {
   document.getElementById('app').appendChild(element); // app 이라 선언된 공간에 컴포넌트 추가.
 }
 
-render();
+render(0);
