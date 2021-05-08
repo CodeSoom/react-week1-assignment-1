ed5d78a39daea75fc450d8c072b1b3a04c19dd64
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-09 07:30:33 +0900

    feat: jsx createElement 함수 생성

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+const createElement = (tagName, props, ...children) => {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+};
