abb0d5d4c0fe342b9e6a8a04e3db032533bdf2c9
developerOlive <chloehiho@gmail.com>
2021-08-11 07:44:37 +0900

    [추가] DOM 강의 내용

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
-/* @jsx createElement */
+function createElement(tagName, ...children) {
+    const element = document.createElement(tagName);
+
+    children.forEach((child) => {
+        element.appendChild(child);
+    });
+    return element;
+}
+
+const paragraph1 = createElement(
+    'p', 
+    document.createTextNode('Hello, future1!'),
+    document.createTextNode('Hello, future2!')
+);
+
+const paragraph2 = createElement(
+    'p',
+    ...[1, 2, 3].map((i) => ( 
+    document.createTextNode(`Hello, Hi! ${i} | `)
+    ))
+);
+
+const root = createElement(
+    'div',
+    paragraph1,
+    paragraph2
+);
+
+const container = document.getElementById('app');
+
+container.appendChild(paragraph1);
+container.appendChild(paragraph2);
