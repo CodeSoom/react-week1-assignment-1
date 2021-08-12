7fb4db81e5202d913141fa178ce0ad387da43fa6
Sehyun Chung <sehyun.chung@icloud.com>
2021-08-12 13:17:31 +0900

    feat: initial implement using `let`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
 
+let count = 0;
+
+function handleClick() {
+  count += 1;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me! (
+          {count}
+          )
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
+
 function createElement(tag, props, ...children) {
   const element = document.createElement(tag);
 
