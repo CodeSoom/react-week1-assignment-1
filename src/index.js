7f1675767ff798f7378480ce0a54619684bc92d3
Sehyun Chung <sehyun.chung@icloud.com>
2021-08-12 19:00:06 +0900

    chore: remove unnecessary codes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function render({ count }) {
   };
 
   const element = (
-    <div id="hello" className="greeting">
+    <div>
       <p>
         <button type="button" onClick={() => handleClickMeButtonClick(count)}>
           Click me! (
