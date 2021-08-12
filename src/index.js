d72f73ac0ce228d11575fd0afb12349fc99985f0
Sehyun Chung <sehyun.chung@icloud.com>
2021-08-12 13:13:44 +0900

    feat: add `createElement`

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tag, props, ...children) {
+  const element = document.createElement(tag);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
