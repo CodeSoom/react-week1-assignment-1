8fcaf3de1b4c694d73da2a505391d7dac329f914
developerOlive <chloehiho@gmail.com>
2021-08-14 00:36:04 +0900

    [리팩토링] esLint fix

--- src/index.js
+++ src/index.js
@@ -5,53 +5,56 @@ const INCREASE = 1;
 const BUTTON = [1, 2, 3];
 
 function createElement(tagName, props, ...children) {
-    const element = document.createElement(tagName);
+  const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase()] = value;
-    });
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    children.flat().forEach((child) => {
-        if (child instanceof Node) {
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    });
-    
-    return element;
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
 }
 
 function render(count) {
-    const handleClick = () => {
-        render(count + INCREASE);
-    }
+  const handleClick = () => {
+    render(count + INCREASE);
+  };
 
-    const handleClickNumberBtn = (number) => {
-        render(number);
-    };
+  const handleClickNumberBtn = (number) => {
+    render(number);
+  };
 
-    const element = (
-        <div id="hello" className="greeting">
-            <p>hello, Olive!</p>
-            <p>
-                <button type="button" onclick={() => handleClick(count)}>
-                    Click me!({count})
-                </button>
-            </p>
+  const element = (
+    <div id="hello" className="greeting">
+      <p>hello, Olive!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          Click me!(
+          {count}
+          )
+        </button>
+      </p>
 
-            <p>
-                {BUTTON.map((number) => (<button type="button" onClick={() => handleClickNumberBtn(number)}>
-                    {number}
-                </button>
-                ))}
-            </p>
-        </div>
-    );
+      <p>
+        {BUTTON.map((number) => (
+          <button type="button" onClick={() => handleClickNumberBtn(number)}>
+            {number}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
 
-    const app = document.getElementById('app');
-    app.textContent = '';
-    app.appendChild(element);
+  const app = document.getElementById('app');
+  app.textContent = '';
+  app.appendChild(element);
 }
 
 render(INIT_COUNT);
