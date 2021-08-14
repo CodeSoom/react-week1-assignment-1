98931299e9a929ec06dd27fc62da7f61e32e5d63
study <dev.chohera@gmail.com>
2021-08-14 12:31:18 +0900

    refact: render함수와 insertElements 함수 분리

--- src/index.js
+++ src/index.js
@@ -2,49 +2,49 @@
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
-  const element = document.createElement(tagName);
+    const element = document.createElement(tagName);
 
-  Object.entries(props || {}).forEach(([key, value]) => {
-    element[key.toLowerCase()] = value;
-  });
+    Object.entries(props || {}).forEach(([key, value]) => {
+      element[key.toLowerCase()] = value;
+    });
 
-  children.flat().forEach((child) => {
-    if (child instanceof Node) {
-      element.appendChild(child);
+    children.flat().forEach((child) => {
+      if (child instanceof Node) {
+        element.appendChild(child);
 
-      return;
-    }
-    element.appendChild(document.createTextNode(child));
-  });
+        return;
+      }
+      element.appendChild(document.createTextNode(child));
+    });
 
-  return element;
-}
+    return element;
+  }
 
-const appElement = document.getElementById('app');
-const startCount = 0;
+  const appElement = document.getElementById('app');
+  const startCount = 0;
 
-function handleClick(count) {
-  const nextCount = count + 1;
-  // eslint-disable-next-line no-use-before-define
-  render(appElement, insertElements, nextCount);
-}
+  function handleClick(count) {
+    const nextCount = count + 1;
+    // eslint-disable-next-line no-use-before-define
+    render(appElement, insertElements, nextCount);
+  }
 
-const insertElements = (countNumber) => (
-  <div id="hello" className="greet">
-    <p>hello world</p>
-    <button type="button" onClick={() => handleClick(countNumber)}>
-      Click me!
-      (
-      {countNumber}
-      )
-    </button>
-  </div>
-);
+  const insertElements = (countNumber) => (
+    <div id="hello" className="greet">
+      <p>hello world</p>
+      <button type="button" onClick={() => handleClick(countNumber)}>
+        Click me!
+        (
+        {countNumber}
+        )
+      </button>
+    </div>
+  );
 
-function render(wrapElement, elements, count) {
-  const container = wrapElement;
-  container.textContent = '';
-  container.appendChild(elements(count));
-}
+  function render(wrapElement, elements, count) {
+    const container = wrapElement;
+    container.textContent = '';
+    container.appendChild(elements(count));
+  }
 
-render(appElement, insertElements, startCount);
+  render(appElement, insertElements, startCount);
