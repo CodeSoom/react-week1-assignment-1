f4da19b1c9851839ed8994ce05380c5e01e20689
padawanR0k <padawanR0k@gmail.com>
2021-11-30 01:08:39 +0900

    new: add component function

--- src/index.js
+++ src/index.js
@@ -14,3 +14,36 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+
+const render = (child) => {
+  const App = document.getElementById('app');
+  if (!App) {
+    return;
+  }
+  App.textContent = '';
+  App.appendChild(child);
+};
+
+const clickHandler = (html) => {
+  render(html);
+};
+
+const component = (count = 0) => (
+  <div>
+    <button type="button" onClick={() => clickHandler(component(count + 1))}>
+      Click me!
+      (
+      {count}
+      )
+    </button>
+    <p>
+      {[1, 2, 3].map((i) => (
+        <button type="button" onClick={() => clickHandler(component(i))}>
+          {i}
+        </button>
+      ))}
+    </p>
+  </div>
+);
+
+render(component());
