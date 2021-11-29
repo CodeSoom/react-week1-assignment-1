9f4c65064132eb1da8793e8ec97bbaadc011b2ef
padawanR0k <padawanR0k@gmail.com>
2021-11-30 01:08:21 +0900

    new: add createElement func

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLocaleLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    const childNode = child instanceof Node ? child : document.createTextNode(child);
+    element.appendChild(childNode);
+  });
+
+  return element;
+}
