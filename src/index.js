986590f132709c83b79b2e732533454a3a2b7774
Ravi <ravi@inflearn.com>
2021-11-30 22:43:18 +0900

    createElement 함수로 화면에 paragraph 추가

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, ...children) {
+  const element = document.createElement(tagName);
+  children.forEach((child) => element.appendChild(child));
+
+  return element;
+}
+
+document
+  .getElementById('app')
+  .appendChild(
+    createElement(
+      'div',
+      createElement(
+        'p',
+        document.createTextNode('Hello World'),
+        document.createTextNode('Hello World'),
+      ),
+      createElement('p', document.createTextNode('hi')),
+    ),
+  );
