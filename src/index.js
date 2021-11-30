db866c75bf9adf3e548c05f3a6a59f6f4570d004
emewjin <emewjin@gmail.com>
2021-11-30 23:47:24 +0900

    up: 강의 내용 및 let 제거

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+const INIT_COUNT = 0;
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const render = (count) => {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => render(count + 1)}>
+          Click Me!
+        </button>
+      </p>
+      <p>
+        {
+          [1, 2, 3].map((i) => <button type="button" onClick={() => render(count + i)}>{i}</button>)
+        }
+      </p>
+      <p>
+        {count}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+};
+
+render(INIT_COUNT);
