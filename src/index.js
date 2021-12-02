7ec282424f0c1bdeaa66e667bafa7a3308d27301
ocipap <papico@inflearn.com>
2021-12-03 00:16:25 +0900

    new: dataset 기반으로 count 값 변경
    * 기존 let 으로 선언되어 있던 값을 돔을 통해 관리하도록 변경

--- src/index.js
+++ src/index.js
@@ -1,2 +1,66 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    const property = key === 'className' ? key : key.toLowerCase();
+    element[property] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const state = {
+  count: 0,
+};
+
+function handleClick(event) {
+  const { target } = event;
+  const root = target.closest('#root');
+  const { dataset: { id } } = root;
+
+  render({ count: id + 1 });
+}
+
+function handleClickNumber(i) {
+  render({ count: i });
+}
+
+function render({ count }) {
+  const element = (
+    <div id="root" className="hello" data-id={count}>
+      <p>Hello, World!</p>
+      <p>
+        <button type="button" onClick={(event) => handleClick(event)}>
+          Click Me!
+          (
+          {state.count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render({ count: 1 });
