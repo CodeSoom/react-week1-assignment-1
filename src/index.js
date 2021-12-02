63ceab8388deac10626f96d3ddb0d5584625663b
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-12-03 03:24:52 +0900

    초기 세팅

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+let count = 0;
+
+function handleClick() {
+  count += 1;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
