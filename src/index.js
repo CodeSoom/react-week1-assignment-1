d7ad1c583ec1da750a48ad038c906784fbae0bce
hy.jang <hy.jang@autowini.com>
2021-12-02 23:03:44 +0900

    let 변수 제거
    App 클래스의 state 멤버 변수의 count 값으로 과제를 진행했습니다.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,74 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLocaleLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+class App {
+  constructor() {
+    this.state = {
+      count: 0,
+    };
+  }
+
+  setCount(newValue) {
+    this.state.count = newValue;
+    this.render();
+  }
+
+  handleClick() {
+    const count = this.state.count + 1;
+    this.setCount(count);
+  }
+
+  handleClickNumber(value) {
+    const count = value;
+    this.setCount(count);
+  }
+
+  render() {
+    const element = (
+      <div id="hello">
+        <p className="greeting">Hello world!</p>
+        <p>
+          <button type="button" onClick={() => this.handleClick()}>
+            Click Me! (
+            {this.state.count}
+            )
+          </button>
+        </p>
+        <p>
+          {[1, 2, 3].map((i) => (
+            <button
+              type="button"
+              onClick={() => {
+                this.handleClickNumber(i);
+              }}
+            >
+              {i}
+            </button>
+          ))}
+        </p>
+      </div>
+    );
+    document.getElementById('app').textContent = '';
+    document.getElementById('app').appendChild(element);
+  }
+}
+
+const app = new App();
+app.render();
