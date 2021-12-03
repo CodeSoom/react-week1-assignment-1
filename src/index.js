5c7386fe6e23cf1f7094fe717fe1afb0134a41d2
jojoldu <jojoldu@gmail.com>
2021-12-03 22:12:05 +0900

    영상 실습 코드 따라하기 완료

--- src/index.js
+++ src/index.js
@@ -1,2 +1,55 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+    const element = document.createElement(tagName);
+
+    Object.entries(props || {}).forEach(([key, value]) => {
+        element[key.toLowerCase()] = value;
+    });
+
+    children.flat().forEach((child) => {
+        if (child instanceof Node) {
+            element.appendChild(child);
+            return;
+        }
+        element.appendChild(document.createTextNode(child));
+    });
+
+    return element;
+}
+
+let count = 0;
+
+function handleClickNumber(value) {
+    count = value;
+    render();
+}
+
+function render() {
+    const element = (
+        <div id="hello" className="greeting">
+            <p>Hello, world!</p>
+            <p>
+                <button type="button" onClick={() => handleClickNumber(count + 1)}>
+                    Click me!
+                    (
+                    {count}
+                    )
+                </button>
+            </p>
+            <p>
+                {[1, 2, 3].map((number) => (
+                    <button type="button" onClick={() => handleClickNumber(number)}>
+                        {number}
+                    </button>
+                ))}
+            </p>
+        </div>
+    );
+
+    document.getElementById('app').textContent = '';
+    document.getElementById('app').appendChild(element);
+}
+
+render();
