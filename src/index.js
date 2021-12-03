46f6291995baa30746e16b724b8b70ef0e20cd09
jojoldu <jojoldu@gmail.com>
2021-12-03 22:18:00 +0900

    let 없애기 완성

--- src/index.js
+++ src/index.js
@@ -2,54 +2,51 @@
 /* @jsx createElement */
 
 function createElement(tagName, props, ...children) {
-    const element = document.createElement(tagName);
+  const element = document.createElement(tagName);
 
-    Object.entries(props || {}).forEach(([key, value]) => {
-        element[key.toLowerCase()] = value;
-    });
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
 
-    children.flat().forEach((child) => {
-        if (child instanceof Node) {
-            element.appendChild(child);
-            return;
-        }
-        element.appendChild(document.createTextNode(child));
-    });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
 
-    return element;
+  return element;
 }
-
-let count = 0;
 
 function handleClickNumber(value) {
-    count = value;
-    render();
+  render(value);
 }
 
-function render() {
-    const element = (
-        <div id="hello" className="greeting">
-            <p>Hello, world!</p>
-            <p>
-                <button type="button" onClick={() => handleClickNumber(count + 1)}>
-                    Click me!
-                    (
-                    {count}
-                    )
-                </button>
-            </p>
-            <p>
-                {[1, 2, 3].map((number) => (
-                    <button type="button" onClick={() => handleClickNumber(number)}>
-                        {number}
-                    </button>
-                ))}
-            </p>
-        </div>
-    );
+function render(count = 0) {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => handleClickNumber(count + 1)}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((number) => (
+          <button type="button" onClick={() => handleClickNumber(number)}>
+            {number}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
 
-    document.getElementById('app').textContent = '';
-    document.getElementById('app').appendChild(element);
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
 }
 
 render();
