f9bd370766c21048d359b9875e31ba5a5c7e4cfe
syaring <sarah.k.serim@gmail.com>
2021-12-05 13:59:33 +0900

    remove let variable

--- src/index.js
+++ src/index.js
@@ -1,2 +1,64 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function render(count = 0) {
+  function handleClick(value) {
+    const newValue = value + 1;
+
+    render(newValue);
+  }
+
+  function handleClickNumber(value) {
+    const newValue = value;
+
+    render(newValue);
+  }
+
+  const element = (
+    <div id="hello" className="greetoing">
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          Click Me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            <p>{i}</p>
+          </button>
+        ))}
+      </p>
+      <p>Hello, world!</p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+
+  document
+    .getElementById('app')
+    .appendChild(element);
+}
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLocaleLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+render();
