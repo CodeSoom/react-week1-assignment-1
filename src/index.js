e7b22f3812c32c8742001c6a189c46eb6474a6ae
이찬호 <0407chan@naver.com>
2022-03-21 11:15:39 +0900

    create - createElement function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
