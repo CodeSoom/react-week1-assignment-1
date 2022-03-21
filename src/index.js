d2db3de8462c3e3adc61ee1fc387587a701a0122
HJUN <guswns823@gmail.com>
2022-03-22 02:21:34 +0900

    [리액트 7기_신현준] 1강 과제 1 - let 제거하기

--- src/index.js
+++ src/index.js
@@ -1,2 +1,65 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+
+class Counter {
+  constructor(count) {
+    this.count = count || 0;
+  }
+
+  handleClickCounter() {
+    this.count += 1;
+    this.render();
+  }
+
+  handleClickNumber(number) {
+    this.count = number;
+    this.render();
+  }
+
+  countElement() {
+    return (
+      <div id="hello" className="greeting">
+        <p>Hello, world!</p>
+        <p>
+          <button type="button" onClick={() => this.handleClickCounter()}>
+            Click me! (
+            {this.count}
+            )
+          </button>
+        </p>
+        <p>
+          {[1, 2, 3].map((i) => (
+            <button type="button" onClick={() => this.handleClickNumber(i)}>
+              {i}
+            </button>
+          ))}
+        </p>
+      </div>
+    );
+  }
+
+  render() {
+    const app = document.getElementById('app');
+    app.textContent = '';
+    app.appendChild(this.countElement());
+  }
+}
+
+const counter = new Counter();
+counter.render();
