5beb161aec283524a78c285e2b6683996cd5886e
free-ko <duddnr654@naver.com>
2022-03-21 21:19:45 +0900

    [1-1] Add : createElement(), render() in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+
+    element.appendChild(document.createTextNode(child));
+  });
+
+  // className이 셋팅이 안된다...머지??
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  return element;
+}
+
+//  머지...let을 사용하지 말라니...
+// const count = {} 객체 안에서 데이터를 재할당해야 하나??
+let count = 0;
+
+function render() {
+  function handleClick() {
+    count += 1;
+    render();
+  }
+
+  function handleClickNumber(value) {
+    count = value;
+    render();
+  }
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, World!</p>
+      <p>
+        <button type="button" onClick={() => handleClick()}>
+          Click me!
+          {count}
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render()
