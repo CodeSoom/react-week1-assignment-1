7f3ccd12e7b7518f08b2b486c13e32d76786af9b
이찬호 <0407chan@naver.com>
2022-03-21 11:16:34 +0900

    add initial 'render', 'handleClick' code

--- src/index.js
+++ src/index.js
@@ -17,3 +17,29 @@ function createElement(tagName, props, ...children) {
 
   return element;
 }
+
+let count = 0;
+
+function handleClick() {
+  count = count + 1;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          ({count})
+        </button>
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
