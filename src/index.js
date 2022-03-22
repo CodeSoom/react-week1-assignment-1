85f1362e7224b81adb1b0c966408405034338891
이찬호 <0407chan@naver.com>
2022-03-22 22:49:13 +0900

    remove duplicate code

--- src/index.js
+++ src/index.js
@@ -43,8 +43,9 @@ function render(count = 0) {
     </div>
   );
 
-  document.getElementById('app').textContent = '';
-  document.getElementById('app').appendChild(element);
+  const app = document.getElementById('app');
+  app.textContent = '';
+  app.appendChild(element);
 }
 
 render();
