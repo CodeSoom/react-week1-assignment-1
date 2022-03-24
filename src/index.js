a6e01a6561f8cc6df88e7e7b54e3a303f32aa757
kjoobin <kjoobin>
2022-03-25 06:12:21 +0900

    Fix using count param & event function move inside render
    use recursive render function with count params

--- src/index.js
+++ src/index.js
@@ -19,22 +19,29 @@ function createElement(tagName, props, ...chlidren) {
   return element;
 }
 
-//
-function render() {
+function render(count = 0) {
+  function handleClick() {
+    render(count + 1);
+  }
+
+  function handleClickNumber(value) {
+    render(value);
+  }
+
   const element = (
     <div id="hello" className="greeting">
       <p>hell worlod</p>
       <p>
-        <button type="button" onClick={() => this.handleClick()}>
+        <button type="button" onClick={() => handleClick()}>
           Click me!
           (
-          {this.count}
+          {count}
           )
         </button>
       </p>
       <p>
         {[1, 2, 3, 4, 5].map((i) => (
-          <button type="button" onClick={() => this.handleClickNumber(i)}>
+          <button type="button" onClick={() => handleClickNumber(i)}>
             {i}
           </button>
         ))}
@@ -48,23 +55,4 @@ function render() {
   container.appendChild(element);
 }
 
-function handleClick() {
-  this.count += 1;
-
-  this.render();
-}
-
-function handleClickNumber(value) {
-  this.count = value;
-
-  this.render();
-}
-
-const counting = {
-  count: 0,
-  handleClick,
-  handleClickNumber,
-  render,
-};
-
-counting.render();
+render();
