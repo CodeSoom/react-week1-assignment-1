9f5a4109dd456bf0122cc60aabd5844e86ca5994
jodheeee <soso99700@naver.com>
2022-05-02 23:29:25 +0900

    1주차_과제1_1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+
+function render(count) {
+  document.getElementById('app').textContent = '';
+  const handleClick = () => {
+    render(count + 1);
+  };
+  const handleClickNumber = (value) => {
+    render(value);
+  };
+
+  document.getElementById('app').appendChild(
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click Me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>,
+  );
+}
+
+render(0);
