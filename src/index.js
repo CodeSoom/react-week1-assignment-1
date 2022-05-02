b48de979a92cc31d840d8fef285d2439b3a57c1b
양탕구리 <rukeni@naver.com>
2022-05-02 18:50:08 +0900

    Refactor : let을 제거하고 함수를 render내부로 이동

--- src/index.js
+++ src/index.js
@@ -1,2 +1,54 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+// TODO: let을 const로 바꾼다.
+const initialCount = 0;
+
+function render(count) {
+  function handleClick() {
+    render(count + 1);
+  }
+  function handleClickNumber(value) {
+    if (Number.isNaN(Number(value))) {
+      return false;
+    }
+    return render(value);
+  }
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello World</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>)}
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+
+  document.getElementById('app').appendChild(element);
+}
+
+render(initialCount);
