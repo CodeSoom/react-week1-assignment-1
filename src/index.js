45e737606a3103a05a34598aef3438ba48f4ff3b
DaengDo <daengdo@saerom.co.kr>
2022-05-02 18:59:01 +0900

    Change let to const
    const로 객체 타입 값을 참조하여 기존의 call by value 를
    call by reference 로 바꿔주었습니다.
    기왕 객체로 만드는 김에 카운트를 증가시키는 함수도 메서드로 넣어줬습니다.
    수업에서 보여준대로 중복되는 dom api를 함수로 묶어주었습니다.
    appendChild() 를 append() 로 변경해주었습니다.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+// let count = 0;
+
+// function handleClick() {
+//   count = count + 1;
+//   render();
+// }
+
+const counter = {
+  count: 0,
+  handleClick() {
+    this.count += 1;
+    render();
+  },
+};
+
+const container = document.getElementById("app");
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={counter.handleClick}>
+          Click me! ({counter.count})
+        </button>
+      </p>
+    </div>
+  );
+
+  container.textContent = "";
+  container.appendChild(element);
+}
+
+render();
