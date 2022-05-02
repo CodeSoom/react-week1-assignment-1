e7e27b49adfe4e9779d100a1b92dcc0f0ef653a7
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-02 14:21:03 +0900

    기능 구현, ESLint, 테스트 통과

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,9 +11,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -29,5 +24,7 @@ module.exports = {
       version: 'latest',
     },
   },
-  rules: {},
+  rules: {
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
 };
