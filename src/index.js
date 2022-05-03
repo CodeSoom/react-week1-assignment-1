cf04a05c01d88cf1097db56bb385e576cbb09db4
hellomargo <developer.margo@gmail.com>
2022-05-03 19:40:05 +0900

    JSX(1-3강)강의들으며 index.js 작성 오늘까지 강의를 들으며 내용을 파악하는 시간을 가졌고, 이론을 학습했습니다. 이제 과제를 진행할 예정입니다.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,54 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+  
+  // console.log(tagName, props, ...children);
+
+  Object.entries(props || {}).forEach(([key, value] )=> {
+    element[key.toLowerCase()] = value;
+  })
+
+  children.flat().forEach((child) => {
+    if(child instanceof Node) {
+      element.appendChild(child);
+      return;
+    } 
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+
+let count = 0;
+
+function handleClick(){
+  count+=1;
+  console.log(count);
+  render();
+}
+
+function handleClickNumber(num) {
+  count = num;
+  render();
+}
+
+function render(){
+  const element = (
+    <div>
+      <p id="hello" className="title">hello world</p>
+      <button type="button" onClick={handleClick}>click!{count}</button>
+      {
+        [1, 2 ,3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+        ))
+      }
+    </div>
+  )
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(
+    element
+  );
+}
+
+render();
