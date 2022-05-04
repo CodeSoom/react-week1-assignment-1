01152d5b2560c19352947dd4cc24eca5971650ba
DaengDo <ring_yoonsuk@naver.com>
2022-05-04 21:00:13 +0900

    Remove reassignment
    어떤 값을 재할당하는 한다는 점에서 결국 객체로 참조하는건 똑같은
    행위였군요
    
    handleClick을 render 함수 안에서 사용하니 더 안전한 느낌도 듭니다.
    이걸 함수 바깥의 변수를 참조하지 않았으니 이런걸 사이드 이펙트가
    줄어들었다고 하나요?
    
    또 검색해보고 querySelector보다 getElementById로 노드를 참조하는게
    속도가 더 빠르다는걸 알게되었습니다.
    
    저는 첫 과제부터 너무 어려웠습니다. 다른분들을 참고하고 검색을 통해 겨우
    해결했습니다....ㅠ

--- src/index.js
+++ src/index.js
@@ -1,2 +1,54 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.append(child);
+      return;
+    }
+    element.append(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+//
+
+function render(count = 0) {
+  function handleClick() {
+    render(count + 1);
+  }
+
+  function handleClickNumber(value) {
+    render(value);
+  }
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me! ({count})
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+  document.getElementById("app").textContent = "";
+  document.getElementById("app").append(element);
+}
+
+render();
