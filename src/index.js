635d2e08c5e55e49712560d6eaae59a043661a5a
Mika <solinepik@gmail.com>
2022-05-05 23:10:41 +0900

    feat: 기초 count 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
-/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension,
+react/jsx-one-expression-per-line */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+let count = 0;
+
+function handleClick() {
+  count += 1;
+  render();
+}
+
+function handleClickNumber(value) {
+  count = value;
+  render();
+}
+
+function render() {
+  const element = (
+    <div id="hello" className="test">
+      <p>Hi</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me! {count}
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  const app = document.getElementById('app');
+  app.textContent = '';
+  app.appendChild(element);
+}
+
+render();
