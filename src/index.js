24d04cf29402ce51bb6a32d12c37525479151ab2
sungho kim <>
2022-06-02 21:49:09 +0900

    첫번째 커밋

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+// ...
+let count = 0;
+
+function handleClick() {
+  count = count + 1;
+  render();
+}
+// ...
+
+// ...
+let count = 0;
+
+function handleClick() {
+  count = count + 1;
+  render();
+}
+// ...
