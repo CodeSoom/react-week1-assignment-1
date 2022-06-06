fa8415f8a7e96235857e78342c00c6b8c9be077f
kimsungjin91 <ksj911011@naver.com>
2022-06-07 02:53:07 +0900

    과제1 - let을 제거해보자
    - no-use-before-define 해결을 위한 render로 함수 이동

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function render(count = 0) {
+  function onUpdateCount(updateCount) {
+    render(updateCount);
+  }
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello. world!</p>
+      <p>
+        <button type="button" onClick={() => onUpdateCount(count + 1)}>
+          click
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => onUpdateCount(i)}>{i}</button>
+        ))}
+      </p>
+    </div>
+  );
+
+  const app = document.getElementById('app');
+  app.innerText = '';
+  app.appendChild(element);
+}
+
+render();
