49b72e32779cbcbb9a7ca069f8efd49e7625d508
Huun <gmldbs0424@gmail.com>
2022-06-06 20:15:55 +0900

    [leehuun] react week1 과제 let 제거하기  - render 함수에 count props 추가.  - render 함수 호출시 count 값으로 연산하도록 로직 변경.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,57 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+    const element = document.createElement(tagName);
+
+    Object.entries(props || {}).forEach(([key, value]) => {
+        element[key.toLocaleLowerCase()] = value;
+    });
+
+    children.flat().forEach((child) => {
+        if(child instanceof Node){
+            element.appendChild(child);
+            return
+        }
+        element.appendChild(document.createTextNode(child));
+    })
+
+    return element;
+}
+
+function handleClick(count) {
+    render(count + 1);
+}
+
+function handleClickNumber(value) {
+    render(value);
+}
+
+// render 호출 시점의 count로 클릭시 연산 수행하도록 변경.
+function render(count) {
+    const element = (
+        <div id="hello" className="greeting">
+            <p>Hello, world!</p>
+            <p>
+                <button type="button" onClick={() => handleClick(count)}>
+                    Click me!
+                    (
+                        {count}
+                    )
+                </button>
+            </p>
+            <p>
+                {
+                    [1,2,3].map((i) => (
+                        <button type="button" onClick={() => handleClickNumber(i)}>{i}</button>
+                    ))
+                }
+            </p>
+        </div>
+    )
+
+    document.getElementById("app").textContent = '';
+    document.getElementById("app").appendChild(element);
+}
+
+// 최초 랜더링시 default count = 0으로 설정.
+render(0);
