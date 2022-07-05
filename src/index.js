42f64c87cda8e65591535fc5ddf2d1a31847c6b5
clapho <clapp301@gmail.com>
2022-07-05 18:14:31 +0900

    1주차 과제1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,41 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+  return element;
+}
+
+//
+
+function handleClick(count) {
+  const newCount = count;
+  render(newCount);
+}
+
+function render(count = 0) {
+  const element = (
+    <div id='hello' className='greeting'>
+      <p>Hello, world!</p>
+      <p>
+        <button type='button' onClick={() => handleClick(count + 1)}>
+          Click me! ({count})
+        </button>
+      </p>
+    </div>
+  );
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+render();
