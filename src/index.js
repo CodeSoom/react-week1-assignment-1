f130f2aaa97cdc879f2a599fe18ea89d692067b3
DESKTOP-NJ8SFNT\moonk <moonkun86@gmail.com>
2022-07-05 20:18:28 +0900

    리액트 과제 첫번째

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+// 함수 생성
+function creatElement(tagName, ...children) {
+    const element = document.creatElement(tagName);
+
+    children.forEach((child) => {
+        element.appendChild(child);
+    });
+
+    return element;
+}
+
+// 실행문
+document.getElementById('app').appendChild( 
+    creatElement(
+        'div',
+        creatElement(
+            'p',
+            ...[1, 2, 3].map((i) =>(
+                document.createTextNode('Hellow, world!'+ i + '|')
+            ))
+        ),
+        creatElement(
+            'p',
+            document.createTextNode('Hi!'),
+        ),
+    ),
+),
