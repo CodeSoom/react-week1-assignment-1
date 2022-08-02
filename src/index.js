61fd77ed1143fd584023a8aca06959140176bb46
ChoJinmok <jjmok1127@gmail.com>
2022-08-02 20:03:20 +0900

    - 1주차 과제 1 let을 사용하지 않고 개발하기 첫번쨰 제출
    재할당을 하지 않고 문제를 해결해야된다고 하셔서 하나의 객체로 묶어서 해결해봤습니다.
    객체 프로퍼티를 갱신하는 것도 재할당에 들어간다면 죄송합니다..

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+const clickHandler = {
+  count: 0,
+  handleClick() {
+    this.count += 1;
+    render();
+  },
+  handleClickNumber(value) {
+    this.count = value;
+    render();
+  },
+};
+
+function render() {
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => clickHandler.handleClick()}>
+          Click me! ({clickHandler.count})
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button
+            type="button"
+            onClick={() => clickHandler.handleClickNumber(i)}
+          >
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById("app").textContent = "";
+  document.getElementById("app").appendChild(element);
+}
+
+render();
