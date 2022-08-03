a7f559d744f1b7ee62c5dccd00a012bd5f0051e9
gunhee-jeong <kyle4619@naver.com>
2022-08-03 20:36:15 +0900

    1주차 과제 1, let을 제거해보자
    - 재할당을 하지 않으면서 기존과 동일한 결과를 만들기 위해서
      함수의 매개변수를 사용하여 문제를 해결해나가고자 생각했습니다.
    - 개발 환경을 구축하고 강의 내용을 이해하는데 많은 시간이 필요했습니다.
      문제를 해결하지 못했지만 commit을 남겨봅니다.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,55 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function render(count = 0) {
+  function handleClick() {
+    //
+    render();
+  }
+
+  function handleClickNumber(value) {
+    //
+    render();
+  }
+
+  const element = (
+    <div>
+      <p className="test">Hello, world@@</p>
+      <p>
+        <button type="button" onClick={handleClick}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      {[1, 2, 3].map((num) => (
+        <button type="button" onClick={() => handleClickNumber}>
+          {num}
+        </button>
+      ))}
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+render();
