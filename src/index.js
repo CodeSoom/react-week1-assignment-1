f829924708fc113147c4265e1cc99cc39711df51
hyeji07 <zindex_ann@naver.com>
2022-10-12 19:33:57 +0900

    remove let

--- src/index.js
+++ src/index.js
@@ -1,2 +1,65 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+//
+const count = {
+  number: 0,
+};
+
+function render() {
+  function handleClick() {
+    count.number += 1;
+    render(count);
+  }
+
+  function handleClickNumber(value) {
+    count.number = value;
+    render(count);
+  }
+
+  const element = (
+    <div id="hello" className="greeting">
+      <p>CoseSoom assignment 1</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          Click me!
+          (
+          {
+              Object.values(count)
+            }
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+
+  document.getElementById('app').textContent = '';
+  document.getElementById('app').appendChild(element);
+}
+
+render();
