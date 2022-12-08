cf995cd54ac67c043c8d389122a86aa79fb309d0
naji0001 <najiwon001@gmail.com>
2022-12-08 18:48:25 +0900

    let 제거
    1. 카운팅 숫자를 매개변수로 전달
    2. 버튼 클릭 이벤트 리스너를 render내부에서 선언

--- src/index.js
+++ src/index.js
@@ -1,2 +1,52 @@
 /* eslint-disable react/react-in-jsx-scope, react/jsx-filename-extension */
 /* @jsx createElement */
+function createElement(tagName, props, ...children) {
+  const element = document.createElement(tagName);
+
+  Object.entries(props || {}).forEach(([key, value]) => {
+    element[key.toLowerCase()] = value;
+  });
+
+  children.flat().forEach((child) => {
+    if (child instanceof Node) {
+      element.appendChild(child);
+      return;
+    }
+    element.appendChild(document.createTextNode(child));
+  });
+
+  return element;
+}
+
+function render(count) {
+  function handleClick(value) {
+    render(value + 1);
+  }
+  function handleClickNumber(value) {
+    render(value);
+  }
+  const element = (
+    <div id="hello" className="greeting">
+      <p>Hello, world!</p>
+      <p>
+        <button type="button" onClick={() => handleClick(count)}>
+          Click me!
+          (
+          {count}
+          )
+        </button>
+      </p>
+      <p>
+        {[1, 2, 3].map((i) => (
+          <button type="button" onClick={() => handleClickNumber(i)}>
+            {i}
+          </button>
+        ))}
+      </p>
+    </div>
+  );
+  document.querySelector('#app').textContent = '';
+  document.querySelector('#app').appendChild(element);
+}
+
+render(0);
